cd85d6aa8649fe78510d56beb088ad0f43991e78
Adeel Farooq <adeelkhattak88088@gmail.com>
2026-08-17 17:09:28 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Logo from './sections/logo'
+import IntroScreen from './components/IntroScreen'
+
+const App = () => {
+  return (
+    <div>
+      <IntroScreen/>
+      <Logo/>
+    </div>
+  )
+}
+
+export default App
